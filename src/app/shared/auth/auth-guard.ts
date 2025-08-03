a6ed9befaff160b7ee103a6653b83e170fdfc91e
bneuhausz <bneuhausz@gmail.com>
2025-08-03 17:57:18 +0200

    added basic auth

---
+++ src/app/shared/auth/auth-guard.ts
@@ -0,0 +1,20 @@
+import { inject } from '@angular/core';
+import { CanActivateFn, Router } from '@angular/router';
+import { Auth } from './auth';
+import { map } from 'rxjs';
+
+export const authGuard = (): CanActivateFn => {
+  return () => {
+    const auth = inject(Auth);
+    const router = inject(Router);
+
+    return auth.firstLoad$.pipe(
+      map(_ => {
+        if (auth.isAuthenticated()) {
+          return true;
+        }
+        return router.parseUrl('/');
+      })
+    );
+  };
+};
--- src/app/shared/auth/auth.ts
+++ src/app/shared/auth/auth.ts
@@ -1,8 +1,98 @@
-import { Injectable } from "@angular/core";
+import { computed, inject, Injectable, signal } from "@angular/core";
+import { AuthError, Session, User } from "@supabase/supabase-js";
+import { Supabase } from "../supabase/supabase";
+import { filter, from, Subject, switchMap, take, tap } from "rxjs";
+import { toObservable } from "@angular/core/rxjs-interop";
+import { Router } from "@angular/router";
+
+type AuthState = {
+  user: User | null;
+  session: Session | null;
+  isLoading: boolean;
+  error: AuthError | null;
+};
 
 @Injectable({
   providedIn: 'root'
 })
-export class AuthService {
-  // Your authentication logic here
+export class Auth {
+  private readonly supabase = inject(Supabase).client;
+  private readonly router = inject(Router);
+
+  readonly #state = signal<AuthState>({
+    user: null,
+    session: null,
+    isLoading: true,
+    error: null
+  });
+
+  readonly user = computed(() => this.#state().user);
+  readonly isAuthenticated = computed(() => !!this.#state().session);
+
+  readonly firstLoad$ = toObservable(this.#state).pipe(
+    filter(state => !state.isLoading),
+    take(1)
+  );
+
+  readonly signIn$ = new Subject<{ email: string, password: string }>();
+  readonly #signIn$ = this.signIn$.pipe(
+    tap(() => this.#setLoading(true)),
+    switchMap(({ email, password }) =>
+      from(this.supabase.auth.signInWithPassword({ email, password }))
+    ),
+    tap(({ data, error }) => {
+      if (error) {
+        this.#setError(error);
+      }
+      else if (data.session) {
+        this.router.navigate(['/']);
+      }
+      else {
+        this.router.navigate(['/confirm-email']);
+      }
+    })
+  );
+
+  readonly signOut$ = new Subject<void>();
+  readonly #signOut$ = this.signOut$.pipe(
+    tap(() => this.#setLoading(true)),
+    switchMap(_ =>
+      from(this.supabase.auth.signOut())
+    ),
+    tap(({ error }) => {
+      if (error) {
+        this.#setError(error);
+      }
+      else {
+        this.router.navigate(['/']);
+      }
+    })
+  );
+
+  constructor() {
+    this.supabase.auth.onAuthStateChange((event, session) => {
+      this.#setAuthStateAfterLogin(session);
+    });
+
+    this.#signIn$.subscribe();
+    this.#signOut$.subscribe();
+  }
+
+  #setError(error: AuthError) {
+    this.#state.update(state => ({ ...state, error }));
+  }
+
+  #setLoading(isLoading: boolean) {
+    this.#state.update(state => ({ ...state, isLoading }));
+  }
+
+  #setAuthStateAfterLogin(session: Session | null) {
+    this.#state.update(state => ({
+      ...state,
+      user: session?.user ?? null,
+      session,
+      isLoading: false,
+      error: null
+    }));
+  }
 }
