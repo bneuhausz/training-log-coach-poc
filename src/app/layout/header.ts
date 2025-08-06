ae3fc105c522279aadedf593faa2eaa47a70c4de
bneuhausz <bneuhausz@gmail.com>
2025-08-06 17:19:00 +0200

    replaced spartan ui with angular material

--- src/app/layout/header.ts
+++ src/app/layout/header.ts
@@ -1,19 +1,19 @@
 import { ChangeDetectionStrategy, Component, inject } from "@angular/core"
-import { HlmButtonDirective } from "@spartan-ng/helm/button";
+import { MatButtonModule } from "@angular/material/button";
 import { Auth } from "../shared/auth/auth";
 import { environment } from "../../environments/environment";
 
 @Component({
   selector: 'app-header',
-  imports: [HlmButtonDirective],
+  imports: [MatButtonModule],
   template: `
     <header>
       @if (auth.isAuthenticated()) {
         <span>Welcome, {{ auth.user()?.email }}</span>
-        <button hlmBtn (click)="auth.signOut$.next()">Logout</button>
+        <button mat-button (click)="auth.signOut$.next()">Logout</button>
       }
       @else {
-        <button hlmBtn (click)="login()">Login</button>
+        <button mat-button (click)="login()">Login</button>
       }
     </header>
   `,
