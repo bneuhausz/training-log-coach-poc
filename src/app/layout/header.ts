59292013d9d339f04b04b51e6e82638c6660ac12
bneuhausz <bneuhausz@gmail.com>
2025-08-06 17:36:08 +0200

    refactored layout

--- src/app/layout/header.ts
+++ src/app/layout/header.ts
@@ -1,26 +1,54 @@
-import { ChangeDetectionStrategy, Component, inject } from "@angular/core"
+import { ChangeDetectionStrategy, Component, inject, input } from "@angular/core"
 import { MatButtonModule } from "@angular/material/button";
 import { Auth } from "../shared/auth/auth";
 import { environment } from "../../environments/environment";
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatSidenav } from "@angular/material/sidenav";
+import { MatIconModule } from "@angular/material/icon";
 
 @Component({
   selector: 'app-header',
-  imports: [MatButtonModule],
+  imports: [MatButtonModule, MatToolbarModule, MatIconModule],
   template: `
-    <header>
+    <mat-toolbar>
       @if (auth.isAuthenticated()) {
-        <span>Welcome, {{ auth.user()?.email }}</span>
-        <button mat-button (click)="auth.signOut$.next()">Logout</button>
+        <button matIconButton (click)="sidenav().open()">
+          <mat-icon id="menu-icon">menu</mat-icon>
+        </button>
+      }
+      <span class="spacer"></span>
+      @if (auth.isAuthenticated()) {
+        <span>{{ auth.user()?.email }}</span>
+        <button matButton="elevated" id="logout-button" (click)="auth.signOut$.next()">Logout</button>
       }
       @else {
-        <button mat-button (click)="login()">Login</button>
+        <button matButton="elevated" (click)="login()">Login</button>
       }
-    </header>
+    </mat-toolbar>
   `,
+  styles: [`
+    .spacer {
+      flex: 1 1 auto;
+    }
+
+    mat-toolbar {
+      background-color: var(--mat-sys-primary);
+      color: var(--mat-sys-on-primary);
+    }
+
+    #logout-button {
+      margin-left: 10px;
+    }
+
+    #menu-icon {
+      color: var(--mat-sys-on-primary);
+    }
+  `],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class Header {
   protected readonly auth = inject(Auth);
+  sidenav = input.required<MatSidenav>();
 
   //TODO: Replace with a proper login form
   protected login() {
--- src/app/layout/layout.ts
+++ src/app/layout/layout.ts
@@ -1,15 +1,21 @@
 import { ChangeDetectionStrategy, Component } from "@angular/core";
 import { RouterOutlet } from "@angular/router";
 import { Header } from "./header";
+import { MatSidenavModule } from "@angular/material/sidenav";
 
 @Component({
   selector: 'app-layout',
-  imports: [RouterOutlet, Header],
+  imports: [RouterOutlet, Header, MatSidenavModule],
   template: `
-    <app-header />
-    <main>
-      <router-outlet />
-    </main>
+    <mat-sidenav-container fullscreen>
+      <mat-sidenav #sidenav>
+      </mat-sidenav>
+      <app-header [sidenav]="sidenav" />
+    
+      <main>
+        <router-outlet />
+      </main>
+    </mat-sidenav-container>
   `,
   changeDetection: ChangeDetectionStrategy.OnPush
 })
