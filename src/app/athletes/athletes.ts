b701a9d948efc0e50cd875e34faee22f807e2a38
bneuhausz <bneuhausz@gmail.com>
2025-08-10 20:03:22 +0200

    added athletes table

--- src/app/athletes/athletes.ts
+++ src/app/athletes/athletes.ts
@@ -1,21 +1,35 @@
 import { ChangeDetectionStrategy, Component, inject } from "@angular/core";
 import { AthletesService } from "./athletes-service";
+import { MatTableModule } from "@angular/material/table";
+import { MatCardModule } from "@angular/material/card";
 
 @Component({
   selector: 'app-athletes',
   providers: [AthletesService],
-  imports: [],
+  imports: [MatCardModule, MatTableModule],
   template: `
-    <h1>Athletes</h1>
-    <p>A list of athletes will be displayed here.</p>
-    @for (athlete of srvc.athletes(); track athlete.id) {
-      <div>
-        <h2>{{ athlete.display_name }}</h2>
-      </div>
-    }
+    <mat-card appearance="outlined" class="w-4/5 my-5 mx-auto">
+      <mat-card-header>
+        <mat-card-title>Athletes</mat-card-title>
+      </mat-card-header>
+
+      <mat-card-content>
+        <table mat-table [dataSource]="srvc.athletes()" class="mat-elevation-z8">
+          <ng-container matColumnDef="display_name">
+            <th mat-header-cell *matHeaderCellDef>Name</th>
+            <td mat-cell *matCellDef="let athlete"> {{ athlete.display_name }} </td>
+          </ng-container>
+
+          <tr mat-header-row *matHeaderRowDef="displayedColumns"></tr>
+          <tr mat-row *matRowDef="let row; columns: displayedColumns;" class="cursor-pointer hover:!bg-primary hover:!text-on-primary"></tr>
+        </table>
+      </mat-card-content>
+    </mat-card>
   `,
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export default class Athletes {
   protected readonly srvc = inject(AthletesService);
+
+  displayedColumns = ['display_name'];
 }
